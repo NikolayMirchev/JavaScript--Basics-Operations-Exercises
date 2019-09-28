0731e8203884222ece7be2bf2b43306a36d28b39
Nikolay Mirchev <48837877+NikolayMirchev@users.noreply.github.com>
2019-09-28 18:36:09 +0300

    Upload some exercise with JavaScript

--- AggregateElements.js
+++ AggregateElements.js
@@ -11,3 +11,27 @@
         
     }
 console.log(solve(1,2,3))
+
+/*
+Aggregate Elements
+Write a program that performs different operations on an array of elements. Implement the following operations:
+•	Sum(ai) - calculates the sum all elements from the input array
+•	Sum(1/ai) - calculates the sum of the inverse values (1/ai) of all elements from the array
+•	Concat(ai) - concatenates the string representations of all elements from the array
+The input comes as an array of number elements.
+The output should be printed on the console on a new line for each of the operations.
+
+Input				Output
+[1, 2, 3]
+Output	
+6
+1.8333
+123	
+
+Input
+[2, 4, 8, 16]	
+Output
+30
+0.9375
+24816
+*/
