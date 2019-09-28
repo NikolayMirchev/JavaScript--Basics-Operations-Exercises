328521aaac766e86f6bca4a9acaa0ca8e572484f
Nikolay Mirchev <48837877+NikolayMirchev@users.noreply.github.com>
2019-09-28 19:25:37 +0300

    Delete TimeToWalk.js

--- TimeToWalk.js
+++
@@ -1,8 +0,0 @@
-function solve(steps,stepL,kmH) {
-    let totalDistance = steps * stepL;
-    let plusMinutes = Math.floor(totalDistance / 500);
-    let totalTime = totalDistance / (kmH / 60);
-
-    return `${h}:${minutes}:${seconds}`
-}
-console.log(solve(4000, 0.60, 5));
